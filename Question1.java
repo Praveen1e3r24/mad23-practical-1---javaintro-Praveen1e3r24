4f6ad6238eea8d77be246247fe562b9620d9489b
Vetriselvan Praveen <116857991+Praveen1e3r24@users.noreply.github.com>
2023-04-20 10:33:04 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,11 +1,6 @@
 public class Question1
 {
-  public static void main(String[] args)
-  {
-    /**
-     * Print "Hello World!"
-     * 
-     * Hint: System.out.println(...)
-     */
+   public static void main(String[] args) {
+    System.out.println("Hello world!");
   }
 }
